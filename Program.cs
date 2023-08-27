49092aab5044eab5a68ef46da8206c9ccc9c988d
osik221 <124447345+osik221@users.noreply.github.com>
2023-08-27 11:13:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rnd = new Random();
+           bool playAgain = true;
+           while (playAgain)
+           {
+                string[] choices = { "WELL", "PAPER", "SCISSORS" };
+                string playerChoice = GetPlayerChoice(choices);
+                string computerChoice = choices[rnd.Next(0, 3)];
+                Console.WriteLine($"Player: {playerChoice}");
+                Console.WriteLine($"Computer: {computerChoice}");
+
+                DetermineTheWinner(playerChoice, computerChoice);
+
+                playAgain = AskToContinue();
+            }
+        }
+        static string GetPlayerChoice(string[] choices)
+        {
+            while (true)
+            {
+                Console.Write("Enter WELL, PAPER, or SCISSORS: ");
+                string playerChoice = Console.ReadLine().ToUpper();
+                if (choices.Contains(playerChoice))
+                {
+                    return playerChoice;
+                }
+                else
+                {
+                    Console.WriteLine("Pls try again:\\");
+                }
+            }
+        }
+        static void DetermineTheWinner(string playerchoice, string computerchoise)
+        {
+            if (playerchoice == computerchoise)
+            {
+                Console.WriteLine("It`s a draw");
+            }
+            else if ((playerchoice == "WELL" && computerchoise == "SCISSORS") ||
+                    (playerchoice == "PAPER" && computerchoise == "WELL") ||
+                    (playerchoice == "Scissors" && computerchoise == "PAPER"))
+            {
+                Console.WriteLine("You won");
+            }else
+            {
+                Console.WriteLine("You lose(!");
+            }
+        }
+        static bool AskToContinue()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to countinue? Yes/No");
+                string aswer = Console.ReadLine().ToUpper();
+                if ((aswer == "YES"))
+                {
+                    return true;
+                }else if ((aswer == "NO"))
+                {
+                    Console.WriteLine("Goodbye!!!");
+                    return false;
+                }
+            }
+        }
+    }
+}
